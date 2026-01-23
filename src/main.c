7036731ad89aa4d1b248677159b6003844820e90
Ivan Mutavdzic <imutavdz@f1r6s7.codam.nl>
2026-01-23 19:39:21 +0100

    main start

--- src/main.c
+++ src/main.c
@@ -6,12 +6,25 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 16:29:24 by imutavdz          #+#    #+#             */
-/*   Updated: 2026/01/23 17:23:08 by imutavdz         ###   ########.fr       */
+/*   Updated: 2026/01/23 19:38:08 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char const *argv[])
+#include "parser.h"
+
+static void	init_g_struct(t_game *game)
 {
+	ft_bzero(game, sizeof(t_game));
+	game->game_over = false;
+}
+
+int	main(int argc, char const *argv[])
+{
+	if (argc != 2)
+		return (print_exit(ERR_ARGS, NULL, false), EXIT_FAILURE);
+	init_g_struct(&game);
+	if (!load_map(&game, argv[1]))
+		return (EXIT_FAILURE);
 	//validation
 	//initialisation
 	//parsing
