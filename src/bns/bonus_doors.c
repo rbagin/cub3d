9a7460627a300fce64d4ae1ad79a6c264a095c43
muta-pro <i-muta@hotmail.com>
2026-02-18 02:02:38 +0100

    open_door check flag

--- src/bns/bonus_doors.c
+++ src/bns/bonus_doors.c
@@ -6,7 +6,7 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 13:58:40 by imutavdz          #+#    #+#             */
-/*   Updated: 2026/02/18 01:34:11 by imutavdz         ###   ########.fr       */
+/*   Updated: 2026/02/18 02:01:24 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,23 @@ void	find_doors(t_game *g)
 	if (count == 0)
 		return ;
 	init_door(g, count);
+}
+
+static bool	open_door(t_game *g, int x, int y)
+{
+	int	i;
+
+	i = pos_door(g, x, y);
+	if (i == -1)
+		return (false);
+	return (g->door[i].open);
+}
+
+bool	hit_door(t_game *g, int x, int y)
+{
+	if (is_out_of_bonds(map, x, y))
+		return (false);
+	if (map->grid[y][x] != 'D')
+		return (false);
+	return (!open_door(g, x, y));
 }
